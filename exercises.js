4c779325f0cccb5fb869e043aff62ab9abc27075
rafael <rafael@rafaels-MBP.T-mobile.com>
2018-01-07 14:20:27 -1000

    completed exercise 1

--- exercises.js
+++ exercises.js
@@ -27,6 +27,8 @@ console.log(testGreeting); // printing the output value of the function.
  * These two variables will be used to invoke the functions #2 - #5.
 */ 
 
+var bango1 = 3;
+var bango2 = 4;
 
 
 /*
